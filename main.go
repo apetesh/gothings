29bd5743baaa37432aff8ddc7619520171ad64d2
Asaf Petesh <asaf@datree.io>
2019-03-06 11:09:51 +0200

    nice and tidy

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-)
-
-type Person struct {
-	Age int     `json:"age"`
-	Name string `json:"name"`
-	Parents struct {
-		Mother &Person
-		Father Person
-	}
-}
 
 func main() {
 
